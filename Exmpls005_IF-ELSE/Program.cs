966e04b67fd4e3401438fd645ec2a99116091c82
Zakhar <ainimwo@gmail.com>
2022-08-28 13:59:20 +0200

    5 коммит IF_ELSE

---
+++ Exmpls005_IF-ELSE/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("ВВЕДИТЕ ИМЯ ПОЛЬЗОВАТЕЛЯ");
+string userName = Console.ReadLine();
+if(userName.ToLower() == "машка")
+{
+    Console.WriteLine("Машка стерва!!!");  
+}
+else
+{
+    Console.WriteLine("Проходи не задерживайся");
+    Console.WriteLine(userName);
+}
+
